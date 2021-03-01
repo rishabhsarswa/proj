329f0007fd1536d55172078ed09397aee49e1703
rishabhsarswa <36982613+rishabhsarswa@users.noreply.github.com>
2021-03-01 13:36:49 +0530

    Add files via upload

--- csh/5/sns/exp_01_00.m
+++ csh/5/sns/exp_01_00.m
@@ -2,17 +2,23 @@
 
 % Experiment 1 : plot impulse , step , sinc function
 
-x = -4*pi:pi/100:4*pi ;
+x = -4*pi:pi/10:4*pi ;
 
 
 vastep = x>0 ;
 
 figure ;
 plot(x,vastep)
-
 xlabel('Time')
 ylabel('Step')
 title('Plot of the Step Function')
+grid
+
+figure ;
+stem(x,vastep)
+xlabel('Time')
+ylabel('Step')
+title('Plot of the Step Discrete Function')
 grid
 
 
@@ -21,28 +27,37 @@ vaimpulse = x==0 ;
 
 figure ;
 plot(x,vaimpulse)
-
 xlabel('Time')
 ylabel('Impulse')
-title('Plot of the Impuse Function')
+title('Plot of the Impulse Function')
 grid
 
+figure ;
+stem(x,vaimpulse)
+xlabel('Time')
+ylabel('Impulse')
+title('Plot of the Impulse Discrete Function')
+grid
+
+
 for(i=1:1:length(x))
-    
     if(x(i) == 0)
         y(i) = 1 ;
     else
         y(i) = sin(x(i))./x(i) ;
     end
-    
 end
 
 figure ;
-
-
 plot(x,y)
-
 xlabel('Time')
 ylabel('sinc(t)')
 title('Plot of the Sinc Function')
+grid
+
+figure ;
+stem(x,y)
+xlabel('Time')
+ylabel('sinc(t)')
+title('Plot of the Sinc Discrete Function')
 grid
